952acbdc7b5cdb74278e444cbe4cd798e71a68fa
Duy Phung <duypv98@gmail.com>
2020-05-20 16:47:32 +0700

    feat: add rotating image animation. fix: layout

--- app/src/main/java/dev/gopro/se/android/mp3clone/MainActivity.java
+++ app/src/main/java/dev/gopro/se/android/mp3clone/MainActivity.java
@@ -2,17 +2,19 @@ package dev.gopro.se.android.mp3clone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.AnimatorInflater;
+import android.animation.ObjectAnimator;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Timer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,20 +22,26 @@ public class MainActivity extends AppCompatActivity {
     public final int PAUSE_IMG = R.mipmap.pause;
 
     private ImageButton prev, play, next;
+    private ImageView imgViewDisk;
     private SeekBar seekBar;
     private MediaPlayer mediaPlayer;
     private TextView songTitle, currentTimeView, totalTimeView;
+
     private Handler seekBarUpdateHandler;
     private Runnable updateSeekBar;
     private MediaPlayer.OnCompletionListener completionListener;
+    private ObjectAnimator rotateAnimator;
+
+
+
 
     public static List<Song> listSong;
     public static int index = 0;
     public static int totalTime = 0;
 
     private List<Song> getListSong() {
-        List<Song> listSong = new ArrayList<Song>();
-        // TODO: get songs in storage ?
+        List<Song> listSong = new ArrayList<>();
+        /* TODO: get songs in storage ? */
         Song song1 = new Song("Co The La Tai Sao (Vu.)", R.raw.cothelataisao);
         Song song2 = new Song("You And Me Alone (Hoaprox x Minh)", R.raw.ngauhung);
         Song song3 = new Song("Dancing On My Own (Pixie Lott ft. GD and TOP", R.raw.dancing_on_my_own);
@@ -50,15 +58,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void init() {
-        this.play = (ImageButton) findViewById(R.id.play_pause_btn);
-        this.prev = (ImageButton) findViewById(R.id.prev_btn);
-        this.next = (ImageButton) findViewById(R.id.next_btn);
-        this.seekBar = (SeekBar) findViewById(R.id.seek_bar);
-        this.songTitle = (TextView) findViewById(R.id.song_title);
+        this.play = findViewById(R.id.play_pause_btn);
+        this.prev = findViewById(R.id.prev_btn);
+        this.next = findViewById(R.id.next_btn);
+        this.seekBar = findViewById(R.id.seek_bar);
+        this.songTitle = findViewById(R.id.song_title);
         this.mediaPlayer = MediaPlayer.create(this, listSong.get(index).getResourceId());
-        this.currentTimeView = (TextView) findViewById(R.id.current_time);
-        this.totalTimeView = (TextView) findViewById(R.id.total_time);
+        this.currentTimeView = findViewById(R.id.current_time);
+        this.totalTimeView = findViewById(R.id.total_time);
+        this.imgViewDisk = findViewById(R.id.dj_disk);
 
+        this.rotateAnimator = (ObjectAnimator) AnimatorInflater.loadAnimator(this, R.animator.rotation);
         this.seekBarUpdateHandler = new Handler();
         this.updateSeekBar = new Runnable() {
             @Override
@@ -81,7 +91,7 @@ public class MainActivity extends AppCompatActivity {
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 if (fromUser) {
                     mediaPlayer.seekTo(progress);
-                    currentTimeView.setText(TimeHandler.miliSecondsToTimer(mediaPlayer.getCurrentPosition()));
+                    currentTimeView.setText(TimeHandler.miliSecondsToTimer(progress));
                 }
             }
 
@@ -103,6 +113,7 @@ public class MainActivity extends AppCompatActivity {
         currentTimeView.setText(TimeHandler.miliSecondsToTimer(0));
         totalTimeView.setText("/   " + TimeHandler.miliSecondsToTimer(totalTime));
         mediaPlayer.setOnCompletionListener(completionListener);
+        rotateAnimator.setTarget(imgViewDisk);
     }
 
 
@@ -126,12 +137,14 @@ public class MainActivity extends AppCompatActivity {
         if (this.mediaPlayer.isPlaying()) {
             this.mediaPlayer.stop();
         }
+        this.rotateAnimator.cancel();
     }
 
     private void createPlayer(int songIndex) {
         this.songTitle.setText(listSong.get(songIndex).getTitle());
         this.mediaPlayer = MediaPlayer.create(this, listSong.get(songIndex).getResourceId());
         this.mediaPlayer.setOnCompletionListener(this.completionListener);
+        this.rotateAnimator.start();
     }
 
     private void switchMediaNext(boolean isNext) {
@@ -146,6 +159,7 @@ public class MainActivity extends AppCompatActivity {
         this.mediaPlayer.start();
         this.play.setImageResource(PAUSE_IMG);
         this.seekBarUpdateHandler.postDelayed(this.updateSeekBar, 1000);
+        this.rotateAnimator.start();
     }
 
     public void play(View view) {
@@ -153,10 +167,16 @@ public class MainActivity extends AppCompatActivity {
             this.mediaPlayer.pause();
             this.seekBarUpdateHandler.removeCallbacks(this.updateSeekBar);
             this.play.setImageResource(PLAY_IMG);
+            this.rotateAnimator.pause();
         } else {
             this.mediaPlayer.start();
             this.seekBarUpdateHandler.postDelayed(this.updateSeekBar, 1000);
             this.play.setImageResource(PAUSE_IMG);
+            if (rotateAnimator.isPaused()) {
+                rotateAnimator.resume();
+            } else {
+                rotateAnimator.start();
+            }
         }
     }
 
